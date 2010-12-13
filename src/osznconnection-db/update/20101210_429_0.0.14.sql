a829df37d024b3254e46b843e50e2d8690ba591b
heaven.iaa <heaven.iaa@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2010-12-13 14:02:35 +0000

    remove db

--- src/osznconnection-db/update/20101210_429_0.0.14.sql
+++
@@ -1,6 +0,0 @@
--- Script renames WRONG_ACCOUNT_NUMBER status description
-
-UPDATE `status_description` SET `name` = 'Не сопоставлен носитель льготы' WHERE `code` = 217;
-
-INSERT INTO `update` (`version`) VALUE ('20101210_429');
-
