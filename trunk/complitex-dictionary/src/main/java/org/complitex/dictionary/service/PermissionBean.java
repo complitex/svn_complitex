d4752dd02f91a15e61af0a1ad20a23ff1f92f568
heaven.iaa <heaven.iaa@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2011-01-26 10:37:28 +0000

    add initial permission

---
+++ trunk/complitex-dictionary/src/main/java/org/complitex/dictionary/service/PermissionBean.java
@@ -0,0 +1,12 @@
+package org.complitex.dictionary.service;
+
+import javax.ejb.Stateless;
+
+/**
+ * @author Anatoly A. Ivanov java@inheaven.ru
+ *         Date: 26.01.11 16:11
+ */
+@Stateless(name = "PermissionBean")
+public class PermissionBean {
+
+}
