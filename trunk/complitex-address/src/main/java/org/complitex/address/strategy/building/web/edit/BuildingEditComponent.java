e47b73621e345d79de0397188f25033070bd4c44
artkof.work <artkof.work@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2011-02-09 07:10:09 +0000

    References of building and organization entities can be observed without security constraints.

--- trunk/complitex-address/src/main/java/org/complitex/address/strategy/building/web/edit/BuildingEditComponent.java
+++ trunk/complitex-address/src/main/java/org/complitex/address/strategy/building/web/edit/BuildingEditComponent.java
@@ -126,7 +126,7 @@ public final class BuildingEditComponent extends AbstractComplexAttributesPanel 
             districtId = districtAttribute.getValueId();
             DomainObject district = null;
             if (districtId != null) {
-                district = districtStrategy.findById(districtId, false);
+                district = districtStrategy.findById(districtId, true);
                 districtComponentState.put("district", district);
             }
         }
