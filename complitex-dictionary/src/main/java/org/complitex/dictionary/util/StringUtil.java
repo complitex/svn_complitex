b914fe01aa613f6d2c69944d931c7e92f5095e79
artkof.work <artkof.work@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2011-03-29 08:40:01 +0000

    TO_CYRILLIC function has been replaced by more advanced implementation. Wherever possible, TO_CYRILLIC function has been replaced by java version of this.

--- complitex-dictionary/src/main/java/org/complitex/dictionary/util/StringUtil.java
+++ complitex-dictionary/src/main/java/org/complitex/dictionary/util/StringUtil.java
@@ -1,11 +1,15 @@
 package org.complitex.dictionary.util;
 
+import com.google.common.collect.ImmutableMap;
+import java.util.Map;
+
 /**
  * @author Anatoly A. Ivanov java@inheaven.ru
  *         Date: 18.08.2010 16:50:37
  */
 public class StringUtil {
-    public static boolean equal(String s1, String s2){
+
+    public static boolean equal(String s1, String s2) {
         return s1 == null && s2 == null || !(s1 == null || s2 == null) && s1.equals(s2);
     }
 
@@ -13,24 +17,86 @@ public class StringUtil {
      * @param object Object
      * @return <code>String.valueOf(object)</code> or empty string if null
      */
-    public static String valueOf(Object object){
+    public static String valueOf(Object object) {
         return object != null ? String.valueOf(object) : "";
     }
 
-    public static String valueOf(Integer sub, Integer all){
-        if (sub != null && all != null){
-            if (all == 0) return sub.toString();
-            return sub + " | " + (100*sub/all) + "%";           
+    public static String valueOf(Integer sub, Integer all) {
+        if (sub != null && all != null) {
+            if (all == 0) {
+                return sub.toString();
+            }
+            return sub + " | " + (100 * sub / all) + "%";
         }
 
         return "";
     }
 
-    public static String getDots(int count){
+    public static String getDots(int count) {
         String dots = "";
 
-        for (int i=0; i<count; ++i) dots += '.';
+        for (int i = 0; i < count; ++i) {
+            dots += '.';
+        }
 
-        return dots; 
+        return dots;
+    }
+    private static final Map<Character, Character> TO_CYRILLIC_MAP = ImmutableMap.<Character, Character>builder().
+            put('a', 'а').
+            put('A', 'А').
+            put('T', 'Т').
+            put('x', 'х').
+            put('X', 'Х').
+            put('k', 'к').
+            put('K', 'К').
+            put('M', 'М').
+            put('e', 'е').
+            put('E', 'Е').
+            put('o', 'о').
+            put('O', 'О').
+            put('p', 'р').
+            put('P', 'Р').
+            put('c', 'с').
+            put('C', 'С').
+            put('B', 'В').
+            put('H', 'Н').
+            put('i', 'і').
+            put('I', 'І').
+            build();
+
+    public static String toCyrillic(String str) {
+        if (str == null) {
+            return null;
+        }
+
+        char[] chars = str.toCharArray();
+        StringBuilder result = new StringBuilder();
+        for (char c : chars) {
+            Character cyrillicCharacter = TO_CYRILLIC_MAP.get(c);
+            if (cyrillicCharacter == null) {
+                result.append(c);
+            } else {
+                result.append(cyrillicCharacter);
+            }
+        }
+        return result.toString();
+    }
+
+    static Map<Character, Character> getToCyrillicMap() {
+        return TO_CYRILLIC_MAP;
+    }
+
+    public static String removeWhiteSpaces(String str) {
+        if (str == null) {
+            return null;
+        }
+        char[] chars = str.toCharArray();
+        StringBuilder result = new StringBuilder();
+        for (char c : chars) {
+            if (c != ' ') {
+                result.append(c);
+            }
+        }
+        return result.toString();
     }
 }
