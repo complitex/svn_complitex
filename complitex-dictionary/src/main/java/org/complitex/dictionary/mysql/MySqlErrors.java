dd3b48352f036aa92e74b17df718b5cca635561d
artkof.work <artkof.work@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2011-04-12 10:44:36 +0000

    inserting of person accounts was took out to separate transaction. Support for local db error handling was added to Abstract LookupPanel. Loading correct `DAT_BEG` field value of ActualPayment was fixed.

--- complitex-dictionary/src/main/java/org/complitex/dictionary/mysql/MySqlErrors.java
+++ complitex-dictionary/src/main/java/org/complitex/dictionary/mysql/MySqlErrors.java
@@ -17,14 +17,18 @@ import org.slf4j.LoggerFactory;
 public final class MySqlErrors {
 
     private static final Logger log = LoggerFactory.getLogger(MySqlErrors.class);
-
     private static final Collection<Integer> INTEGRITY_CONSTRAINT_VIOLATION_ERROR_CODES = ImmutableSet.of(
             630, 839, 840, 893, 1062, 1169, 1215, 1216, 1217, 1451, 1452, 1557);
+    private static final int DUBLICATE_ERROR_CODE = 1062;
 
     private MySqlErrors() {
     }
 
     public static boolean isIntegrityConstraintViolationError(SQLException exception) {
         return INTEGRITY_CONSTRAINT_VIOLATION_ERROR_CODES.contains(exception.getErrorCode());
+    }
+
+    public static boolean isDublicateError(SQLException exception) {
+        return DUBLICATE_ERROR_CODE == exception.getErrorCode();
     }
 }
