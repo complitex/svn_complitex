4767f6890ad2c4a501e356a22733ae2c95e6ad19
artkof.work <artkof.work@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2011-04-14 09:53:41 +0000

    resolving street part of payment address was improved.

--- complitex-db/complitex-testdump.sql
+++ complitex-db/complitex-testdump.sql
@@ -42,13 +42,14 @@ insert into apartment_attribute(attribute_id, object_id, attribute_type_id, valu
 
 -- Building Addresses
 insert into building_address(object_id, parent_id, parent_entity_id) values (1,1,300), (2,3,300), (3,1,300), (4,1,300), (5,2,300), (6,2,300),
-(7,4,300), (8,5,300);
---(9,6,300);
+(7,4,300), (8,5,300),
+(9,6,300);
 insert into building_address_string_culture(id, locale_id, value) values
 (1, 1, UPPER('8')), (2, 1, UPPER('28')), (3,1,UPPER('18')), (4,1,UPPER('12')), (5,1,UPPER('21')), (6,1,UPPER('100')),
 (1, 2, UPPER('8')), (2, 2, UPPER('28')), (3,2,UPPER('18')), (4,2,UPPER('12')), (5,2,UPPER('21')), (6,2,UPPER('100')),
-(7,1,UPPER('154А')), (7,2,UPPER('154А')),(8,1,UPPER('25А')), (8,2,UPPER('25А'));
---(9,1,UPPER('25А')), (9,2,UPPER('25А')), (10,1,UPPER('к1')), (10,2,UPPER('к1'));
+(7,1,UPPER('154А')), (7,2,UPPER('154А')),(8,1,UPPER('25А')), (8,2,UPPER('25А')),
+(9,1,UPPER('26А'));
+-- (10,1,UPPER('к1')), (10,2,UPPER('к1'));
 insert into building_address_attribute(attribute_id, object_id, attribute_type_id, value_id, value_type_id) values
 (1,1,1500,1,1500),
 (1,2,1500,6,1500),
@@ -57,38 +58,38 @@ insert into building_address_attribute(attribute_id, object_id, attribute_type_i
 (1,5,1500,4,1500),
 (1,6,1500,5,1500),
 (1,7,1500,7,1500),
-(1,8,1500,8,1500);
---(1,9,1500,9,1500),
---(1,9,1501,10,1501);
+(1,8,1500,8,1500),
+(1,9,1500,9,1500),
+(1,9,1501,10,1501);
 
 -- Buildings
-insert into building(object_id, parent_id, parent_entity_id) values (1,1,1500), (2,3,1500), (3,4,1500), (4,5,1500), (5,6,1500), (6,7,1500), (7,8,1500);
---    (8,9,1500);
+insert into building(object_id, parent_id, parent_entity_id) values (1,1,1500), (2,3,1500), (3,4,1500), (4,5,1500), (5,6,1500), (6,7,1500), (7,8,1500),
+(8,9,1500);
 insert into building_attribute(attribute_id, object_id, attribute_type_id, value_id, value_type_id) values
 (1,1,500,2,500),(1,1,501,2,501),
 (1,3,500,2,500),
 (1,4,500,1,500),
 (1,5,500,1,500),
 (1,6,500,3,500),
-(1,7,500,3,500);
---(1,8,500,3,500);
+(1,7,500,3,500),
+(1,8,500,3,500);
 
 -- Streets
 insert into street_string_culture(id, locale_id, value) values (1, 1, UPPER('Терешковой')), (1,2,UPPER('Tereshkovoy')),
                                                             (2, 1, UPPER('Ленина')), (2,2,UPPER('Lenina')),
                                                             (3, 1, UPPER('Морской')), (3,2, UPPER('Morskoy')),
                                                             (4, 1, UPPER('КОСИОРА')), (4,2, UPPER('КОСИОРА')),
-                                                            (5, 1, UPPER('ФРАНТИШЕКА КРАЛА')), (5,2, UPPER('ФРАНТИШЕКА КРАЛА'));
---                                                            (6, 1, UPPER('ФРАНТИШЕКА КРАЛА')), (6,2, UPPER('ФРАНТИШЕКА КРАЛА'));
-insert into street(object_id, parent_id, parent_entity_id) values (1,1,400), (2,2,400), (3,1,400), (4,3,400), (5,3,400);
---(6,3,400);
+                                                            (5, 1, UPPER('ФРАНТИШЕКА КРАЛА')), (5,2, UPPER('ФРАНТИШЕКА КРАЛА')),
+                                                            (6, 1, UPPER('ФРАНТИШЕКА КРАЛА')), (6,2, UPPER('ФРАНТИШЕКА КРАЛА'));
+insert into street(object_id, parent_id, parent_entity_id) values (1,1,400), (2,2,400), (3,1,400), (4,3,400), (5,3,400),
+(6,3,400);
 insert into street_attribute(attribute_id, object_id, attribute_type_id, value_id, value_type_id) values
 (1,1,300,1,300),(1,1,301,10012,301),
 (1,2,300,2,300),(1,2,301,10012,301),
 (1,3,300,3,300),(1,3,301,10008,301),
 (1,4,300,4,300),(1,4,301,10012,301),
-(1,5,300,5,300),(1,5,301,10012,301);
---(1,6,300,6,300),(1,6,301,10008,301);
+(1,5,300,5,300),(1,5,301,10012,301),
+(1,6,300,6,300),(1,6,301,10008,301);
 
 -- Districts
 insert into district_string_culture(id, locale_id, value) values (1, 1, UPPER('Ленинский')), (1, 2,UPPER('Leninsky')),
