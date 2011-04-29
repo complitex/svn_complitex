12bc333c4b74b049187573a0016da063b918533f
artkof.work <artkof.work@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2011-04-29 10:51:36 +0000

    Gender simple type was introduced and applied to person entity

--- complitex-dictionary/src/main/java/org/complitex/dictionary/web/component/type/BooleanPanel.java
+++ complitex-dictionary/src/main/java/org/complitex/dictionary/web/component/type/BooleanPanel.java
@@ -4,15 +4,19 @@
  */
 package org.complitex.dictionary.web.component.type;
 
+import java.util.Locale;
 import org.apache.wicket.markup.html.form.CheckBox;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
+import org.complitex.dictionary.util.ResourceUtil;
 
 /**
  *
  * @author Artem
  */
 public class BooleanPanel extends Panel {
+
+    private static final String RESOURCE_BUNDLE = BooleanPanel.class.getName();
 
     public BooleanPanel(String id, IModel<Boolean> model, IModel<String> labelModel, boolean enabled) {
         super(id);
@@ -21,5 +25,13 @@ public class BooleanPanel extends Panel {
         checkBox.setEnabled(enabled);
         checkBox.setLabel(labelModel);
         add(checkBox);
+    }
+
+    public static String display(boolean value, Locale locale) {
+        return ResourceUtil.getString(RESOURCE_BUNDLE, resourceKey(value), locale);
+    }
+
+    private static String resourceKey(boolean value) {
+        return String.valueOf(value);
     }
 }
