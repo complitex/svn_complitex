b3e252e855b6b8197ab28234b293bb75cfa89f83
artkof.work <artkof.work@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2011-05-04 04:20:17 +0000

    History support was added to persons.

--- complitex-template/src/main/java/org/complitex/template/web/pages/HistoryPage.java
+++ complitex-template/src/main/java/org/complitex/template/web/pages/HistoryPage.java
@@ -7,7 +7,7 @@ package org.complitex.template.web.pages;
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.authorization.strategies.role.annotations.AuthorizeInstantiation;
 import org.complitex.dictionary.strategy.web.HistoryPanel;
-import org.complitex.template.strategy.TemplateStrategy;
+import static org.complitex.template.strategy.TemplateStrategy.*;
 import org.complitex.template.web.security.SecurityRole;
 import org.complitex.template.web.template.TemplatePage;
 
@@ -16,10 +16,14 @@ import org.complitex.template.web.template.TemplatePage;
  * @author Artem
  */
 @AuthorizeInstantiation(SecurityRole.AUTHORIZED)
-public final class HistoryPage extends TemplatePage {
+public class HistoryPage extends TemplatePage {
 
     public HistoryPage(PageParameters params) {
-        add(new HistoryPanel("historyPanel", params.getString(TemplateStrategy.ENTITY), params.getAsLong(TemplateStrategy.OBJECT_ID)));
+        add(newHistoryPanel("historyPanel", params.getString(STRATEGY), params.getString(ENTITY), params.getAsLong(OBJECT_ID)));
+    }
+
+    protected HistoryPanel newHistoryPanel(String id, String strategyName, String entity, long objectId) {
+        return new HistoryPanel(id, strategyName, entity, objectId);
     }
 }
 
