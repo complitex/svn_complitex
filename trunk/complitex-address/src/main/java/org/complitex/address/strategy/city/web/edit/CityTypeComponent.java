a80e4644424ec2a10e4b3bb14b341e1a5d8eae8d
artkof.work <artkof.work@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2011-05-13 09:06:50 +0000

    Street type and city type attribute labels were changed, displaying types was corrected.

--- trunk/complitex-address/src/main/java/org/complitex/address/strategy/city/web/edit/CityTypeComponent.java
+++ trunk/complitex-address/src/main/java/org/complitex/address/strategy/city/web/edit/CityTypeComponent.java
@@ -4,6 +4,8 @@
  */
 package org.complitex.address.strategy.city.web.edit;
 
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.ResourceModel;
 import org.complitex.dictionary.strategy.web.AbstractComplexAttributesPanel;
 import org.complitex.dictionary.web.component.EntityTypePanel;
@@ -25,8 +27,20 @@ public class CityTypeComponent extends AbstractComplexAttributesPanel {
     @Override
     protected void init() {
         final DomainObject city = getDomainObject();
-        EntityTypePanel cityType = new EntityTypePanel("cityType", "city_type", CityTypeStrategy.NAME, city, CityStrategy.CITY_TYPE,
-                new ResourceModel("city_type"), !isDisabled() && DomainObjectAccessUtil.canEdit(null, "city", city));
+        IModel<Long> cityTypeModel = new Model<Long>() {
+
+            @Override
+            public Long getObject() {
+                return city.getAttribute(CityStrategy.CITY_TYPE).getValueId();
+            }
+
+            @Override
+            public void setObject(Long object) {
+                city.getAttribute(CityStrategy.CITY_TYPE).setValueId(object);
+            }
+        };
+        EntityTypePanel cityType = new EntityTypePanel("cityType", "city_type", CityTypeStrategy.NAME, cityTypeModel,
+                new ResourceModel("city_type"), true, !isDisabled() && DomainObjectAccessUtil.canEdit(null, "city", city));
         add(cityType);
     }
 }
--- trunk/complitex-address/src/main/java/org/complitex/address/strategy/street/web/edit/StreetTypeComponent.java
+++ trunk/complitex-address/src/main/java/org/complitex/address/strategy/street/web/edit/StreetTypeComponent.java
@@ -4,6 +4,8 @@
  */
 package org.complitex.address.strategy.street.web.edit;
 
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 import org.apache.wicket.model.ResourceModel;
 import org.complitex.dictionary.strategy.web.AbstractComplexAttributesPanel;
 import org.complitex.dictionary.web.component.EntityTypePanel;
@@ -24,9 +26,21 @@ public class StreetTypeComponent extends AbstractComplexAttributesPanel {
 
     @Override
     protected void init() {
-        DomainObject street = getDomainObject();
-        EntityTypePanel streetType = new EntityTypePanel("streetType", "street_type", StreetTypeStrategy.NAME, street, StreetStrategy.STREET_TYPE,
-                new ResourceModel("street_type"), !isDisabled() && DomainObjectAccessUtil.canEdit(null, "street", street));
+        final DomainObject street = getDomainObject();
+        IModel<Long> streetTypeModel = new Model<Long>() {
+
+            @Override
+            public Long getObject() {
+                return street.getAttribute(StreetStrategy.STREET_TYPE).getValueId();
+            }
+
+            @Override
+            public void setObject(Long object) {
+                street.getAttribute(StreetStrategy.STREET_TYPE).setValueId(object);
+            }
+        };
+        EntityTypePanel streetType = new EntityTypePanel("streetType", "street_type", StreetTypeStrategy.NAME, streetTypeModel,
+                new ResourceModel("street_type"), true, !isDisabled() && DomainObjectAccessUtil.canEdit(null, "street", street));
         add(streetType);
     }
 }
