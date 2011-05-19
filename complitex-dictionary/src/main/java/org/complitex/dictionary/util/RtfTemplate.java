ecbff746f0475a2b80ce82a30900a5bb05566a97
heaven.iaa <heaven.iaa@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2011-05-19 10:25:04 +0000

    add initial pspoffice report export module

---
+++ complitex-dictionary/src/main/java/org/complitex/dictionary/util/RtfTemplate.java
@@ -0,0 +1,118 @@
+package org.complitex.dictionary.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Anatoly A. Ivanov java@inheaven.ru
+ *         Date: 11.05.11 17:45
+ */
+public class RtfTemplate {
+    private final static Logger log = LoggerFactory.getLogger(RtfTemplate.class);
+
+    public final static Charset CHARSET1251 = Charset.forName("Windows-1251");
+
+    public static class RtfTemplateException extends RuntimeException {
+        public RtfTemplateException(String message, Throwable cause) {
+            super(message, cause);
+        }
+    }
+
+    private final static Pattern VARIABLE_PATTERN = Pattern.compile("%%(\\S+)%%", Pattern.DOTALL | Pattern.MULTILINE);
+
+    private StringBuilder template = new StringBuilder(8192);
+
+    private Map<String, String> map = new HashMap<String, String>();
+
+    public RtfTemplate(InputStream inputStream) {
+        if (!(inputStream instanceof BufferedInputStream)) {
+            inputStream = new BufferedInputStream(inputStream);
+        }
+
+        Reader reader = new InputStreamReader(inputStream, CHARSET1251);
+
+        try {
+            for (int c; (c = reader.read()) != -1;) {
+                template.append((char) c);
+            }
+        } catch (IOException e) {
+            throw new RtfTemplateException("Ошибка чтения файла rtf шаблона", e);
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                log.debug("Ошибка закрытия потока", e);
+            }
+        }
+    }
+
+    public RtfTemplate addValue(String key, String value) {
+        map.put(key, value);
+
+        return this;
+    }
+
+    public RtfTemplate addValues(Map<String, String> values) {
+        map.putAll(values);
+
+        return this;
+    }
+
+    public String fill() {
+        if (map.isEmpty()) {
+            return template.toString();
+        }
+
+        StringBuffer result = new StringBuffer(template.length());
+        Matcher matcher = VARIABLE_PATTERN.matcher(template);
+
+        while (matcher.find()) {
+            String value = map.get(matcher.group(1));
+
+            if (value != null) {
+                matcher.appendReplacement(result, getHexEscape(Matcher.quoteReplacement(value).getBytes(CHARSET1251)));
+            }
+        }
+
+        matcher.appendTail(result);
+
+        return result.toString();
+    }
+
+    public void fill(OutputStream outputStream){
+        try {
+            outputStream.write(fill().getBytes(CHARSET1251));
+        } catch (IOException e) {
+            throw new RtfTemplateException("Ошибка записи в поток", e);
+        } finally {
+            try {
+                outputStream.close();
+            } catch (IOException e) {
+                log.debug("Ошибка закрытия потока", e);
+            }
+        }
+    }
+
+    private static final String HEXES = "0123456789abcdef";
+
+    private String getHexEscape(byte[] raw) {
+        if (raw == null) {
+            return null;
+        }
+
+        StringBuilder hex = new StringBuilder(2 * raw.length);
+
+        for (byte b : raw) {
+            hex.append("\\\\'").append(HEXES.charAt((b & 0xF0) >> 4)).append(HEXES.charAt((b & 0x0F)));
+        }
+
+        return hex.toString();
+    }
+}
