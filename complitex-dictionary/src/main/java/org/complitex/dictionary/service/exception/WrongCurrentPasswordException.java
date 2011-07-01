09b2b1cb1ff2fbc37753c19dc695b87ddca441e7
heaven.iaa <heaven.iaa@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2011-07-01 12:10:19 +0000

    add user profile

---
+++ complitex-dictionary/src/main/java/org/complitex/dictionary/service/exception/WrongCurrentPasswordException.java
@@ -0,0 +1,8 @@
+package org.complitex.dictionary.service.exception;
+
+/**
+ * @author Anatoly A. Ivanov java@inheaven.ru
+ *         Date: 01.07.11 17:53
+ */
+public class WrongCurrentPasswordException extends Exception{
+}
