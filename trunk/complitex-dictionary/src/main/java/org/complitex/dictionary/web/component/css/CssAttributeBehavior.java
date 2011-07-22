f260e22373ea58c01b1c513b654e4058851ab484
artkof.work <artkof.work@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2011-07-22 10:36:19 +0000

    Out of date AutoCompleteTextField has been removed as well as old SearchComponent's stuffs, disappearing of district address compoennt parts at editing of building has been fixed.

---
+++ trunk/complitex-dictionary/src/main/java/org/complitex/dictionary/web/component/css/CssAttributeBehavior.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.complitex.dictionary.web.component.css;
+
+import static com.google.common.collect.Sets.*;
+import java.util.Set;
+import org.apache.wicket.Component;
+import org.apache.wicket.behavior.AbstractBehavior;
+import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.util.string.Strings;
+
+/**
+ *
+ * @author Artem
+ */
+public class CssAttributeBehavior extends AbstractBehavior {
+
+    private Set<String> cssClasses = newLinkedHashSet();
+
+    public CssAttributeBehavior(String cssClass) {
+        cssClasses.add(cssClass);
+    }
+
+    public CssAttributeBehavior(Set<String> cssClasses) {
+        this.cssClasses.addAll(cssClasses);
+    }
+
+    @Override
+    public void onComponentTag(Component component, ComponentTag tag) {
+        for (String className : cssClasses) {
+            if (!Strings.isEmpty(className)) {
+                CharSequence oldClassName = tag.getString("class");
+                if (Strings.isEmpty(oldClassName)) {
+                    tag.put("class", className);
+                } else {
+                    tag.put("class", oldClassName + " " + className);
+                }
+            }
+        }
+    }
+}
