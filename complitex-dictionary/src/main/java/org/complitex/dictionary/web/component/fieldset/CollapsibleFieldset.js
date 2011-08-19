abf04033e7ccd0283f5eb8e99e528a434d78c2f9
artkof.work <artkof.work@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2011-08-19 05:11:22 +0000

    Collapsible fieldset has been tuned.

---
+++ complitex-dictionary/src/main/java/org/complitex/dictionary/web/component/fieldset/CollapsibleFieldset.js
@@ -0,0 +1,28 @@
+/**
+ *
+ * @author Artem
+ */
+
+(function( $ ){
+    $.fn.extend({
+        collapsible_fieldset: function(){
+            return this.each(function(){
+                var fieldset = $(this).find("fieldset.collapsible_fs");
+                var legend = fieldset.children("legend");
+                var content = fieldset.children(".fs_content");
+                var image = legend.find(".fs_image");
+                if(image.hasClass("plus")){
+                    content.hide();
+                }
+                legend.bind("click", function(){
+                    if(image.hasClass("minus")){
+                        image.removeClass("minus").addClass("plus");
+                    } else if(image.hasClass("plus")){
+                        image.removeClass("plus").addClass("minus");
+                    }
+                    content.toggle("fast");
+                });
+            });
+        }
+    });
+})( jQuery );
