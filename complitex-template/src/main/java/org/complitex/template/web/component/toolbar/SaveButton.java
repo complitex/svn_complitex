f4b2f7646f53ee2e146e537d2356b131d21de132
artkof.work <artkof.work@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2011-10-06 05:35:44 +0000

    Psp office work in progress. Generation report mechanism has been reorganized. Two reports FamilyAndApartmentInfo and FamilyAndHousingPayments have been enabled.

--- complitex-template/src/main/java/org/complitex/template/web/component/toolbar/SaveButton.java
+++ complitex-template/src/main/java/org/complitex/template/web/component/toolbar/SaveButton.java
@@ -1,8 +1,6 @@
 package org.complitex.template.web.component.toolbar;
 
 import org.apache.wicket.ResourceReference;
-import org.apache.wicket.ajax.markup.html.AjaxLink;
-import org.apache.wicket.markup.html.link.Link;
 
 /**
  * @author Anatoly A. Ivanov java@inheaven.ru
@@ -11,7 +9,7 @@ import org.apache.wicket.markup.html.link.Link;
 public abstract class SaveButton extends ToolbarButton {
 
     private static final String IMAGE_SRC = "images/icon-save.gif";
-    private static final String TITLE_KEY = "title";
+    private static final String TITLE_KEY = "image.title.save";
 
     public SaveButton(String id, boolean useAjax) {
         super(id, new ResourceReference(IMAGE_SRC), TITLE_KEY, useAjax);
