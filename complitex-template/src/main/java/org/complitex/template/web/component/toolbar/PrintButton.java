e8596a1919991db0014531e015eda89420860da8
artkof.work <artkof.work@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2011-11-25 08:09:41 +0000

    Psp office work in progress. Possibility to open pdf reports directly in browser(print reports) rather through external reader has been added.

---
+++ complitex-template/src/main/java/org/complitex/template/web/component/toolbar/PrintButton.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.complitex.template.web.component.toolbar;
+
+import org.apache.wicket.ResourceReference;
+
+/**
+ *
+ * @author Artem
+ */
+public class PrintButton extends ToolbarButton {
+
+    private static final String IMAGE_SRC = "images/icon-print.gif";
+    private static final String TITLE_KEY = "image.title.print";
+
+    public PrintButton(String id, boolean useAjax) {
+        super(id, new ResourceReference(IMAGE_SRC), TITLE_KEY, useAjax);
+    }
+}
