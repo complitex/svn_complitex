ba1527e38345c31b792a068288a4bed4713ac91a
artkof.work <artkof.work@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2012-03-22 03:40:49 +0000

    "Organization type" attribute type id has been changed from 905 to 904 in order to make organizations attribute types more consistent.

--- complitex-dictionary/src/main/java/org/complitex/dictionary/strategy/organization/IOrganizationStrategy.java
+++ complitex-dictionary/src/main/java/org/complitex/dictionary/strategy/organization/IOrganizationStrategy.java
@@ -14,38 +14,99 @@ import org.complitex.dictionary.strategy.IStrategy;
  */
 public interface IOrganizationStrategy extends IStrategy {
 
-    /**
+    /*
      * Attribute type ids
      */
+    /**
+     * Organization name.
+     */
     long NAME = 900;
+    /**
+     * Organization's code.
+     */
     long CODE = 901;
+    /**
+     * District reference.
+     */
     long DISTRICT = 902;
+    /**
+     * User organization's parent.
+     */
     long USER_ORGANIZATION_PARENT = 903;
-    long ORGANIZATION_TYPE = 905;
+    /**
+     * Organization type.
+     */
+    long ORGANIZATION_TYPE = 904;
 
     /**
-     * Gets user organizations that current user can see (at least reading).
+     * Returns user organizations that current user can see (at least to read).
      * That is, it returns all user organizations that accessed by user's organizations.
      * 
      * @param locale Locale. It is used in sorting of user organizations by name.
      * @param excludeOrganizationsId Ids of user organizations that should be excluded from returned list.
-     * @return 
+     * @return User organizations that current user can see.
      */
     @Transactional
     List<? extends DomainObject> getUserOrganizations(Locale locale, Long... excludeOrganizationsId);
 
+    /**
+     * Returns set of user organization object's ids that descendant of user organization with id of {@code parentOrganizationId}
+     * regardless of visibility of organization objects to current user, i.e. it works for any user the same way as for admin user.
+     * 
+     * @param parentOrganizationId Id of given user organization object.
+     * @return Ids of all descendant user organizations.
+     */
     @Transactional
     Set<Long> getTreeChildrenOrganizationIds(long parentOrganizationId);
 
+    /**
+     * Returns code of district object associated with given {@code organization} 
+     * or <code>null</code> if {@code organization} doesn't support reference to district.
+     * 
+     * @param organization Organization object.
+     * @return District code of organization or <code>null</code>.
+     */
     String getDistrictCode(DomainObject organization);
 
+    /**
+     * Calculates whether given {@code organization} play role of user organization, i.e. has user organization type
+     * in set of organizations type attributes (recall that organization may play role of more one organization type at the same time).
+     * 
+     * @param organization Organization object.
+     * @return true if {@code organization} is user organization.
+     */
     boolean isUserOrganization(DomainObject organization);
 
+    /**
+     * Validates code of organization for existing. 
+     * <p>
+     * Note: 
+     * If tested organization is new then {@code id} is <code>null</code>.
+     * </p>
+     * 
+     * @param id Id of tested organization.
+     * @param code Code of tested organization.
+     * @return Id of any existing organization with the same code but another id if such exists and <code>null</code> otherwise.
+     */
     @Transactional
     Long validateCode(Long id, String code);
 
+    /**
+     * Validates name of organization for existing.
+     * 
+     * @param id Id of tested organization.
+     * @param name Name of tested organization.
+     * @param locale Locale. It is used to validate only for names in given {@code locale}.
+     * @return Id of any existing organization with the same name but another id if such exists and <code>null</code> otherwise.
+     */
     @Transactional
     Long validateName(Long id, String name, Locale locale);
 
+    /**
+     * Returns code of {@code organization}.
+     * 
+     * @param organization Organization.
+     * @return Organization's code.
+     */
     String getUniqueCode(DomainObject organization);
 }
