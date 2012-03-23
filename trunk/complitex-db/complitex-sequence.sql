21c6a0114e8d05e4f229a470d1b4161e755440bc
artkof.work <artkof.work@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2012-03-23 10:31:58 +0000

    Toolbar buttons for adding new buildings, apartment and rooms has been added.

--- trunk/complitex-db/complitex-sequence.sql
+++ trunk/complitex-db/complitex-sequence.sql
@@ -25,3 +25,5 @@ update sequence set sequence_value = (select IFNULL(max(`id`), 0) from `organiza
 update sequence set sequence_value = (select IFNULL(max(`object_id`), 0) from `organization`)+1 where sequence_name = 'organization';
 update sequence set sequence_value = (select IFNULL(max(`id`), 0) from `organization_type_string_culture`)+1 where sequence_name = 'organization_type_string_culture';
 update sequence set sequence_value = (select IFNULL(max(`object_id`), 0) from `organization_type`)+1 where sequence_name = 'organization_type';
+update sequence set sequence_value = (select IFNULL(max(`id`), 0) from `user_info_string_culture`)+1 where sequence_name = 'user_info_string_culture';
+update sequence set sequence_value = (select IFNULL(max(`object_id`), 0) from `user_info`)+1 where sequence_name = 'user_info';
