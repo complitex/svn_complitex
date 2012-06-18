078b6dba0b0c8761bd0187ee28a8f0056ee4701f
artkof.work <artkof.work@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2012-06-18 09:27:20 +0000

    DatePicker's image fix.

--- complitex-dictionary/src/main/java/org/complitex/dictionary/web/component/DatePicker.java
+++ complitex-dictionary/src/main/java/org/complitex/dictionary/web/component/DatePicker.java
@@ -20,53 +20,30 @@ public class DatePicker<T> extends org.odlabs.wiquery.ui.datepicker.DatePicker<T
     private static final String RELATIVE_IMAGE_SRC = "images/calendar.gif";
     private static IConverter<Date> CONVERTER = new PatternDateConverter("dd.MM.yyyy", true);
 
-    public DatePicker(String id, boolean enabled) {
-        super(id);
-        init(enabled);
-    }
-
     public DatePicker(String id) {
         super(id);
-        init(true);
-    }
-
-    public DatePicker(String id, Class<T> type, boolean enabled) {
-        super(id, type);
-        init(enabled);
     }
 
     public DatePicker(String id, Class<T> type) {
         super(id, type);
-        init(true);
-    }
-
-    public DatePicker(String id, IModel<T> model, boolean enabled) {
-        super(id, model);
-        init(enabled);
     }
 
     public DatePicker(String id, IModel<T> model) {
         super(id, model);
-        init(true);
-    }
-
-    public DatePicker(String id, IModel<T> model, Class<T> type, boolean enabled) {
-        super(id, model, type);
-        init(enabled);
     }
 
     public DatePicker(String id, IModel<T> model, Class<T> type) {
         super(id, model, type);
-        init(true);
     }
 
-    protected void init(boolean enabled) {
-        if (enabled) {
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+        if (isEnabled()) {
             setButtonImage(getImageUrl(getRequestCycle()));
             setShowOn(ShowOnEnum.BOTH);
             setButtonImageOnly(true);
         }
-        setEnabled(enabled);
         setDateFormat("dd.mm.yy");
     }
 
