6b12ba8b3ab54015961e09faed1a2253bf1adbcd
heaven.iaa <heaven.iaa@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2012-09-27 06:28:20 +0000

    add payload import

--- trunk/complitex-dictionary/src/main/java/org/complitex/dictionary/entity/IImportFile.java
+++ trunk/complitex-dictionary/src/main/java/org/complitex/dictionary/entity/IImportFile.java
@@ -1,10 +1,12 @@
 package org.complitex.dictionary.entity;
 
+import java.io.Serializable;
+
 /**
  * @author Anatoly A. Ivanov java@inheaven.ru
  *         Date: 01.03.11 18:58
  */
-public interface IImportFile {
+public interface IImportFile extends Serializable {
     public String getFileName();
 
     public String name();
