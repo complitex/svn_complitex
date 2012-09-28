915a689b3ec0d5e47a0db1f312ab88e2065cdcd1
heaven.iaa <heaven.iaa@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2012-09-28 11:22:07 +0000

    add code change for heatmeter edit page

--- complitex-dictionary/src/main/java/org/complitex/dictionary/web/component/search/ISearchCallback.java
+++ complitex-dictionary/src/main/java/org/complitex/dictionary/web/component/search/ISearchCallback.java
@@ -7,13 +7,14 @@ package org.complitex.dictionary.web.component.search;
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 
+import java.io.Serializable;
 import java.util.Map;
 
 /**
  *
  * @author Artem
  */
-public interface ISearchCallback {
+public interface ISearchCallback extends Serializable{
 
     void found(Component component, Map<String, Long> ids, AjaxRequestTarget target);
 }
