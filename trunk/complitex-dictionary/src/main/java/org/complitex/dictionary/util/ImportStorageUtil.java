92e3a68c3d3522a90478b865600021fdf24f63c7
heaven.iaa <heaven.iaa@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2012-10-11 11:35:07 +0000

    add payload link

--- trunk/complitex-dictionary/src/main/java/org/complitex/dictionary/util/ImportStorageUtil.java
+++ trunk/complitex-dictionary/src/main/java/org/complitex/dictionary/util/ImportStorageUtil.java
@@ -3,7 +3,6 @@ package org.complitex.dictionary.util;
 import au.com.bytecode.opencsv.CSVReader;
 import nl.knaw.dans.common.dbflib.CorruptedTableException;
 import nl.knaw.dans.common.dbflib.Table;
-import org.complitex.dictionary.entity.IImportFile;
 import org.complitex.dictionary.service.exception.ImportFileNotFoundException;
 import org.complitex.dictionary.service.exception.ImportFileReadException;
 
@@ -18,54 +17,54 @@ public final class ImportStorageUtil {
     private ImportStorageUtil() {
     }
 
-    public static CSVReader getCsvReader(String dir, IImportFile file, String charsetName, char separator)
+    public static CSVReader getCsvReader(String dir, String fileName, String charsetName, char separator)
             throws ImportFileNotFoundException {
         try {
             return new CSVReader(new InputStreamReader(new FileInputStream(
-                    new File(dir, file.getFileName())), charsetName), separator, '"', 1);
+                    new File(dir, fileName)), charsetName), separator, '"', 1);
         } catch (Exception e) {
-            throw new ImportFileNotFoundException(e, file.getFileName());
+            throw new ImportFileNotFoundException(e, fileName);
         }
     }
 
-    public static InputStream getInputStream(String dir, IImportFile file) throws ImportFileNotFoundException {
+    public static InputStream getInputStream(String dir, String fileName) throws ImportFileNotFoundException {
         try {
-            return new BufferedInputStream(new FileInputStream(new File(dir, file.getFileName())));
+            return new BufferedInputStream(new FileInputStream(new File(dir, fileName)));
         } catch (FileNotFoundException e) {
-            throw new ImportFileNotFoundException(e, file.getFileName());
+            throw new ImportFileNotFoundException(e, fileName);
         }
     }
 
-    public static Table getDbfTable(String dir, IImportFile file, String charsetName)
+    public static Table getDbfTable(String dir, String fileName, String charsetName)
             throws ImportFileNotFoundException, ImportFileReadException {
         try {
-            Table table = new Table(new File(dir, file.getFileName()), charsetName);
+            Table table = new Table(new File(dir, fileName), charsetName);
             table.open();
 
             return table;
         } catch (IOException e) {
-            throw new ImportFileNotFoundException(e, file.getFileName());
+            throw new ImportFileNotFoundException(e, fileName);
         } catch (CorruptedTableException e) {
-            throw new ImportFileReadException(e, file.getFileName(), -1);
+            throw new ImportFileReadException(e, fileName, -1);
         }
     }
 
-    public static int getRecordCount(String dir, IImportFile file)
+    public static int getRecordCount(String dir, String fileName)
             throws ImportFileNotFoundException, ImportFileReadException {
-        switch (file.getFileName().substring(file.getFileName().lastIndexOf('.') + 1).toLowerCase()){
+        switch (fileName.substring(fileName.lastIndexOf('.') + 1).toLowerCase()){
             case "csv":
-                return getCsvRecordCount(dir, file);
+                return getCsvRecordCount(dir, fileName);
             case "dbf":
-                return getDbfRecordCount(dir, file);
+                return getDbfRecordCount(dir, fileName);
         }
 
         throw new IllegalArgumentException("File extension must be 'csv' or 'dbf");
     }
 
-    public static int getCsvRecordCount (String dir, IImportFile file)
+    public static int getCsvRecordCount (String dir, String fileName)
             throws ImportFileNotFoundException, ImportFileReadException {
         try {
-            LineNumberReader reader = new LineNumberReader(new FileReader(new File(dir, file.getFileName())));
+            LineNumberReader reader = new LineNumberReader(new FileReader(new File(dir, fileName)));
 
             String line;
             int index = -1;
@@ -78,23 +77,23 @@ public final class ImportStorageUtil {
 
             return index;
         } catch (FileNotFoundException e) {
-            throw new ImportFileNotFoundException(e, file.getFileName());
+            throw new ImportFileNotFoundException(e, fileName);
         } catch (IOException e) {
-            throw new ImportFileReadException(e, file.getFileName(), -1);
+            throw new ImportFileReadException(e, fileName, -1);
         }
     }
 
-    public static int getDbfRecordCount (String dir, IImportFile file)
+    public static int getDbfRecordCount (String dir, String fileName)
             throws ImportFileNotFoundException, ImportFileReadException {
         try {
-            Table table = new Table(new File(dir, file.getFileName()));
+            Table table = new Table(new File(dir, fileName));
             table.open();
 
             return table.getRecordCount();
         } catch (CorruptedTableException e) {
-            throw new ImportFileReadException(e, file.getFileName(), -1);
+            throw new ImportFileReadException(e, fileName, -1);
         } catch (IOException e) {
-            throw new ImportFileNotFoundException(e, file.getFileName());
+            throw new ImportFileNotFoundException(e, fileName);
         }
     }
 
