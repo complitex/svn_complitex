dc78afa691166e0b620009e6c2746d2e65bbac68
heaven.iaa <heaven.iaa@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2012-12-12 07:54:51 +0000

    add heatmeter modification  check

---
+++ trunk/complitex-dictionary/src/main/java/org/complitex/dictionary/service/exception/ConcurrentModificationException.java
@@ -0,0 +1,8 @@
+package org.complitex.dictionary.service.exception;
+
+/**
+ * @author Anatoly A. Ivanov java@inheaven.ru
+ *         Date: 12.12.12 12:16
+ */
+public class ConcurrentModificationException extends Exception{
+}
