1f34295aac82166041ddd649617080856183f6e5
heaven.iaa <heaven.iaa@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2012-12-13 07:29:00 +0000

    fix tablegram import begin om

--- trunk/complitex-dictionary/src/main/java/org/complitex/dictionary/web/component/LabelTextField.java
+++ trunk/complitex-dictionary/src/main/java/org/complitex/dictionary/web/component/LabelTextField.java
@@ -6,15 +6,24 @@ import org.apache.wicket.behavior.Behavior;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.util.convert.IConverter;
+import org.complitex.dictionary.converter.BigDecimalConverter;
+
+import java.math.BigDecimal;
 
 /**
  * @author Anatoly A. Ivanov java@inheaven.ru
  *         Date: 28.11.12 16:06
  */
 public class LabelTextField<T> extends Panel {
+    private final static IConverter<BigDecimal> BIG_DECIMAL_CONVERTER_2 = new BigDecimalConverter(2);
+    private final static IConverter<BigDecimal> BIG_DECIMAL_CONVERTER_7 = new BigDecimalConverter(7);
+
+    public enum Converter {NONE, BIG_DECIMAL_CONVERTER_2, BIG_DECIMAL_CONVERTER_7}
+
     private TextField<T> textField;
 
-    public LabelTextField(String id, int size, IModel<T> model) {
+    public LabelTextField(String id, int size, IModel<T> model, final Converter converter) {
         super(id, model);
 
         add(new TextLabel("label", model){
@@ -29,11 +38,28 @@ public class LabelTextField<T> extends Panel {
             public boolean isVisible() {
                 return LabelTextField.this.isEnabled();
             }
+
+            @SuppressWarnings("unchecked")
+            @Override
+            public <C> IConverter<C> getConverter(Class<C> type) {
+                switch (converter) {
+                    case BIG_DECIMAL_CONVERTER_2:
+                        return (IConverter<C>) BIG_DECIMAL_CONVERTER_2;
+                    case BIG_DECIMAL_CONVERTER_7:
+                        return (IConverter<C>) BIG_DECIMAL_CONVERTER_7;
+                }
+
+                return super.getConverter(type);
+            }
         });
 
         if (size > 0){
             textField.add(new AttributeModifier("size", size));
         }
+    }
+
+    public LabelTextField(String id, int size, IModel<T> model) {
+        this(id, size, model, Converter.NONE);
     }
 
     @Override
