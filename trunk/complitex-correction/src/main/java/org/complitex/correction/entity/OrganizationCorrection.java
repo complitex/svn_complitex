2d4c2f1fcc5315450d78b972d7cee874648e2509
heaven.iaa <heaven.iaa@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2013-12-03 21:37:53 +0000

    add  filter and sort by servicing organization on subsidy list page

--- trunk/complitex-correction/src/main/java/org/complitex/correction/entity/OrganizationCorrection.java
+++ trunk/complitex-correction/src/main/java/org/complitex/correction/entity/OrganizationCorrection.java
@@ -7,6 +7,14 @@ import org.complitex.dictionary.entity.Correction;
  *         Date: 28.11.13 15:41
  */
 public class OrganizationCorrection extends Correction {
+    public OrganizationCorrection() {
+    }
+
+    public OrganizationCorrection(String externalId, Long objectId, String correction, Long organizationId,
+                                  Long userOrganizationId, Long moduleId) {
+        super(externalId, objectId, correction, organizationId, userOrganizationId, moduleId);
+    }
+
     @Override
     public String getEntity() {
         return "organization";
