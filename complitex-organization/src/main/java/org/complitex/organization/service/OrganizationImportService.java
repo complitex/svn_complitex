fdfdb0b6d8828686918eed91077ca2630300f0b4
heaven.iaa <heaven.iaa@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2013-12-04 14:26:15 +0000

    add sum column to subsidy list

--- complitex-organization/src/main/java/org/complitex/organization/service/OrganizationImportService.java
+++ complitex-organization/src/main/java/org/complitex/organization/service/OrganizationImportService.java
@@ -28,7 +28,6 @@ import java.util.Queue;
 import static org.complitex.dictionary.strategy.organization.IOrganizationStrategy.*;
 import static org.complitex.organization.entity.OrganizationImportFile.ORGANIZATION;
 import static org.complitex.organization_type.strategy.OrganizationTypeStrategy.SERVICING_ORGANIZATION;
-import static org.complitex.organization_type.strategy.OrganizationTypeStrategy.USER_ORGANIZATION_TYPE;
 
 @Stateless
 public class OrganizationImportService extends AbstractImportService {
@@ -177,7 +176,7 @@ public class OrganizationImportService extends AbstractImportService {
     private void addOrganizationTypes(DomainObject organization) {
         organization.removeAttribute(ORGANIZATION_TYPE);
         organization.addAttribute(newOrganizationTypeAttribute(1L, SERVICING_ORGANIZATION));
-        organization.addAttribute(newOrganizationTypeAttribute(2L, USER_ORGANIZATION_TYPE));
+        //organization.addAttribute(newOrganizationTypeAttribute(2L, USER_ORGANIZATION_TYPE));
     }
 
     private Attribute newOrganizationTypeAttribute(long attributeId, long organizationTypeId) {
