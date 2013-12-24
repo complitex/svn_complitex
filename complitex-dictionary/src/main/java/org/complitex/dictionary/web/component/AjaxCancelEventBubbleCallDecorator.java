4894c6d460f4120f9ae7d7353ffc7cd9ae31de67
heaven.iaa <heaven.iaa@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2013-12-24 13:53:03 +0000

    fix firefox stop bubble

--- complitex-dictionary/src/main/java/org/complitex/dictionary/web/component/AjaxCancelEventBubbleCallDecorator.java
+++ complitex-dictionary/src/main/java/org/complitex/dictionary/web/component/AjaxCancelEventBubbleCallDecorator.java
@@ -3,12 +3,14 @@ package org.complitex.dictionary.web.component;
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.IAjaxCallDecorator;
 import org.apache.wicket.ajax.calldecorator.AjaxPostprocessingCallDecorator;
+import org.apache.wicket.markup.html.IComponentAwareHeaderContributor;
+import org.apache.wicket.markup.html.IHeaderResponse;
 
 /**
  * @author Anatoly Ivanov java@inheaven.ru
  *         Date: 10.12.13 17:45
  */
-public class AjaxCancelEventBubbleCallDecorator extends AjaxPostprocessingCallDecorator{
+public class AjaxCancelEventBubbleCallDecorator extends AjaxPostprocessingCallDecorator implements IComponentAwareHeaderContributor {
     public AjaxCancelEventBubbleCallDecorator(){
         this(null);
     }
@@ -19,6 +21,21 @@ public class AjaxCancelEventBubbleCallDecorator extends AjaxPostprocessingCallDe
 
     @Override
     public CharSequence postDecorateScript(Component component, CharSequence script){
-        return "e = window.event; if(e.stopPropagation) {e.stopPropagation();}else{e.cancelBubble = true;}" + script;
+        return "stopBubble(event);" + script;
+    }
+
+    @Override
+    public void renderHead(Component component, IHeaderResponse response) {
+        response.renderJavaScript("function stopBubble(event) {\n" +
+                "    if (event && event.stopPropagation) {\n" +
+                "        event.stopPropagation();\n" +
+                "    } \n" +
+                "    else if (window.event) {\n" +
+                "        window.event.cancelBubble = true;\n" +
+                "    }\n" +
+                "    else if (window.$.Event.prototype) {\n" +
+                "        window.$.Event.prototype.stopPropagation();\n" +
+                "    }\n" +
+                "}", "stopBubble");
     }
 }
