3f7a6b8f6f6c169f4f719a1d8e6997f58ddd058e
heaven.iaa <heaven.iaa@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2014-01-29 20:29:31 +0000

    add district select panel

--- complitex-organization/src/main/java/org/complitex/organization/web/component/OrganizationPickerPanel.java
+++ complitex-organization/src/main/java/org/complitex/organization/web/component/OrganizationPickerPanel.java
@@ -14,16 +14,14 @@ import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.markup.repeater.data.DataView;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
-import org.apache.wicket.util.template.PackageTextTemplate;
-import org.apache.wicket.util.template.TextTemplate;
 import org.complitex.dictionary.entity.DomainObject;
-import org.complitex.dictionary.entity.example.AttributeExample;
 import org.complitex.dictionary.entity.example.DomainObjectExample;
 import org.complitex.dictionary.service.LocaleBean;
 import org.complitex.dictionary.strategy.organization.IOrganizationStrategy;
 import org.complitex.dictionary.util.AttributeUtil;
 import org.complitex.dictionary.web.component.datatable.DataProvider;
 import org.complitex.dictionary.web.component.paging.PagingNavigator;
+import org.complitex.dictionary.web.model.AttributeExampleModel;
 
 import javax.ejb.EJB;
 import java.util.List;
@@ -35,8 +33,6 @@ import static org.complitex.dictionary.strategy.organization.IOrganizationStrate
  *         Date: 28.01.14 18:49
  */
 public class OrganizationPickerPanel extends Panel {
-    private static final TextTemplate CENTER_DIALOG_JS = new PackageTextTemplate(OrganizationPickerPanel.class, "CenterDialog.js");
-
     @EJB
     private LocaleBean localeBean;
 
@@ -53,66 +49,31 @@ public class OrganizationPickerPanel extends Panel {
         final Form<Void> filterForm = new Form<Void>("filterForm");
         content.add(filterForm);
 
-        final DomainObjectExample example = new DomainObjectExample();
-        example.addAttributeExample(new AttributeExample(NAME));
-        example.addAttributeExample(new AttributeExample(CODE));
+        //Example
+        final DomainObjectExample example = new DomainObjectExample(NAME, CODE);
+
         if (organizationTypeIds != null && !organizationTypeIds.isEmpty()) {
             example.addAdditionalParam(ORGANIZATION_TYPE_PARAMETER, organizationTypeIds);
         }
 
-        final DataProvider<DomainObject> dataProvider = new DataProvider<DomainObject>() {
+        if (isBalanceHolder()){
+            example.addAdditionalParam(IOrganizationStrategy.BALANCE_HOLDER_PARAMETER, true);
+        }
 
-            @Override
-            protected Iterable<? extends DomainObject> getData(int first, int count) {
-                example.setLocaleId(localeBean.convert(getLocale()).getId());
-                example.setStart(first);
-                example.setSize(count);
-
-                return organizationStrategy.find(example);
-            }
-
-            @Override
-            protected int getSize() {
-                example.setLocaleId(localeBean.convert(getLocale()).getId());
-                return organizationStrategy.count(example);
-            }
-        };
-
-        filterForm.add(new TextField<>("nameFilter", new Model<String>() {
-            @Override
-            public String getObject() {
-                return example.getAttributeExample(NAME).getValue();
-            }
-
-            @Override
-            public void setObject(String name) {
-                example.getAttributeExample(NAME).setValue(name);
-            }
-        }));
-
-        filterForm.add(new TextField<>("codeFilter", new Model<String>() {
-            @Override
-            public String getObject() {
-                return example.getAttributeExample(CODE).getValue();
-            }
-
-            @Override
-            public void setObject(String code) {
-                example.getAttributeExample(CODE).setValue(code);
-            }
-        }));
+        filterForm.add(new TextField<>("nameFilter", new AttributeExampleModel(example, NAME)));
+        filterForm.add(new TextField<>("codeFilter", new AttributeExampleModel(example, CODE)));
 
         final RadioGroup<Long> radioGroup = new RadioGroup<>("radioGroup", organizationModel);
         radioGroup.add(new AjaxFormChoiceComponentUpdatingBehavior() {
 
             @Override
             protected void onUpdate(AjaxRequestTarget target) {
-               //update model
+                //update model
             }
         });
         filterForm.add(radioGroup);
 
-        DataView<DomainObject> data = new DataView<DomainObject>("data", dataProvider) {
+        DataView<DomainObject> data = new DataView<DomainObject>("data", getDataProvider(example)) {
 
             @Override
             protected void populateItem(Item<DomainObject> item) {
@@ -142,6 +103,30 @@ public class OrganizationPickerPanel extends Panel {
             }
         };
         filterForm.add(find);
+    }
+
+    protected DataProvider<DomainObject> getDataProvider(final DomainObjectExample example) {
+        return new DataProvider<DomainObject>() {
+
+                @Override
+                protected Iterable<? extends DomainObject> getData(int first, int count) {
+                    example.setLocaleId(localeBean.convert(getLocale()).getId());
+                    example.setStart(first);
+                    example.setSize(count);
+
+                    return organizationStrategy.find(example);
+                }
+
+                @Override
+                protected int getSize() {
+                    example.setLocaleId(localeBean.convert(getLocale()).getId());
+                    return organizationStrategy.count(example);
+                }
+            };
+    }
+
+    protected boolean isBalanceHolder(){
+        return false;
     }
 
     protected void onFind(AjaxRequestTarget target){
