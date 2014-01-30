71745ac73eded6bcec6ccd05c040b2d12331029e
heaven.iaa <heaven.iaa@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2014-01-30 20:24:00 +0000

    add organization multiselect panel

--- complitex-organization/src/main/java/org/complitex/organization/web/component/OrganizationMultiselectPanel.java
+++ complitex-organization/src/main/java/org/complitex/organization/web/component/OrganizationMultiselectPanel.java
@@ -3,6 +3,7 @@ package org.complitex.organization.web.component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Check;
 import org.apache.wicket.markup.html.form.CheckGroup;
@@ -17,6 +18,7 @@ import org.complitex.dictionary.entity.DomainObject;
 import org.complitex.dictionary.strategy.organization.IOrganizationStrategy;
 
 import javax.ejb.EJB;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -27,51 +29,57 @@ public class OrganizationMultiselectPanel extends Panel{
     @EJB(name = IOrganizationStrategy.BEAN_NAME, beanInterface = IOrganizationStrategy.class)
     private IOrganizationStrategy organizationStrategy;
 
-    public OrganizationMultiselectPanel(String id, final IModel<List<DomainObject>> model) {
+    public OrganizationMultiselectPanel(String id, final IModel<List<DomainObject>> model, List<Long> organizationTypeIds) {
         super(id);
 
-        final CheckGroup<DomainObject> checkGroup = new CheckGroup<>("check_group", new ListModel<DomainObject>());
-        checkGroup.setOutputMarkupId(true);
-        add(checkGroup);
+        final WebMarkupContainer container = new WebMarkupContainer("container");
+        container.setOutputMarkupId(true);
+        container.setOutputMarkupPlaceholderTag(true);
+        add(container);
+
+        final CheckGroup<DomainObject> checkGroup = new CheckGroup<>("check_group", new ListModel<>(new ArrayList<DomainObject>()));
+        container.add(checkGroup);
 
         ListView<DomainObject> listView = new ListView<DomainObject>("list_view", model){
             @Override
             protected void populateItem(ListItem<DomainObject> item) {
                 String name = organizationStrategy.displayShortNameAndCode(item.getModelObject(), getLocale());
 
-                item.add(new Check<>("check"));
+                item.add(new Check<>("check", Model.of(item.getModelObject())));
                 item.add(new Label("name", Model.of(name)));
             }
         };
         checkGroup.add(listView);
 
+        final OrganizationSelectPanel organizationSelectPanel = new OrganizationSelectPanel("organization_select", organizationTypeIds){
+            @Override
+            protected void onSelect(AjaxRequestTarget target, DomainObject domainObject) {
+                model.getObject().add(domainObject);
 
-        add(new AjaxLink("add") {
+                target.add(container.setVisible(true));
+                target.add(setVisible(false));
+            }
+        };
+        organizationSelectPanel.setOutputMarkupId(true);
+        organizationSelectPanel.setOutputMarkupPlaceholderTag(true);
+        organizationSelectPanel.setVisible(false);
+        add(organizationSelectPanel);
+
+        container.add(new AjaxLink("add") {
             @Override
             public void onClick(AjaxRequestTarget target) {
-
+                target.add(container.setVisible(false));
+                target.add(organizationSelectPanel.setVisible(true));
             }
         });
 
-        add(new AjaxSubmitLink("delete") {
+        container.add(new AjaxSubmitLink("delete") {
             @Override
             protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
                 model.getObject().removeAll(checkGroup.getModelObject());
 
-                target.add(checkGroup);
+                target.add(container);
             }
         });
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
