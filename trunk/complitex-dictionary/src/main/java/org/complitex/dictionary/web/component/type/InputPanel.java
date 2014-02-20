25c83154549db3bc95f126dec090de16289d7133
pavel.sknar@gmail.com <pavel.sknar@gmail.com@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2014-02-20 12:35:18 +0000

    1. Added visualization container to IConfig. 2. Using OrganizationPicker in config edit.

--- trunk/complitex-dictionary/src/main/java/org/complitex/dictionary/web/component/type/InputPanel.java
+++ trunk/complitex-dictionary/src/main/java/org/complitex/dictionary/web/component/type/InputPanel.java
@@ -4,6 +4,7 @@
  */
 package org.complitex.dictionary.web.component.type;
 
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.MarkupContainer;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
@@ -32,6 +33,20 @@ public class InputPanel<T> extends Panel {
     }
 
     /**
+     * For use in non-ajax environment
+     * @param id
+     * @param model
+     * @param type
+     * @param required
+     * @param labelModel
+     * @param enabled
+     */
+    public InputPanel(String id, IModel<T> model, Class<T> type, boolean required, IModel<String> labelModel, boolean enabled, Integer size) {
+        super(id);
+        init(model, type, required, labelModel, enabled, null, size);
+    }
+
+    /**
      * For use in ajax environment
      * @param id
      * @param model
@@ -46,12 +61,35 @@ public class InputPanel<T> extends Panel {
         init(model, type, required, labelModel, enabled, toUpdate);
     }
 
+    /**
+     * For use in ajax environment
+     * @param id
+     * @param model
+     * @param type
+     * @param required
+     * @param labelModel
+     * @param enabled
+     * @param toUpdate
+     * @param size
+     */
+    public InputPanel(String id, IModel<T> model, Class<T> type, boolean required, IModel<String> labelModel, boolean enabled, MarkupContainer[] toUpdate, Integer size) {
+        super(id);
+        init(model, type, required, labelModel, enabled, toUpdate, size);
+    }
+
     protected void init(IModel<T> model, Class<T> type, boolean required, IModel<String> labelModel, boolean enabled, final MarkupContainer[] toUpdate) {
+        init(model, type, required, labelModel, enabled, toUpdate, null);
+    }
+
+    protected void init(IModel<T> model, Class<T> type, boolean required, IModel<String> labelModel, boolean enabled, final MarkupContainer[] toUpdate, Integer size) {
         TextField<T> textField = new TextField<T>("textField", model);
         textField.setType(type);
         textField.setEnabled(enabled);
         textField.setLabel(labelModel);
         textField.setRequired(required);
+        if (size != null && size > 0) {
+            textField.add(new AttributeModifier("size", String.valueOf(size)));
+        }
         if (toUpdate != null) {
             textField.add(new AjaxFormComponentUpdatingBehavior("onchange") {
 
