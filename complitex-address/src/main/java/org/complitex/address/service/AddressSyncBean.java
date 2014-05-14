071008845ba3392c0ae1b5e4788e1efcc5cb750e
heaven.iaa <heaven.iaa@7bb6860a-f51d-f1f6-0ad8-69df8237b33a>
2014-05-14 19:10:21 +0000

    add address sync bean

--- complitex-address/src/main/java/org/complitex/address/service/AddressSyncBean.java
+++ complitex-address/src/main/java/org/complitex/address/service/AddressSyncBean.java
@@ -1,10 +1,37 @@
 package org.complitex.address.service;
 
+import org.complitex.address.entity.AbstractAddressSync;
+import org.complitex.dictionary.entity.FilterWrapper;
 import org.complitex.dictionary.service.AbstractBean;
+
+import java.util.List;
 
 /**
  * @author Anatoly A. Ivanov java@inheaven.ru
  *         Date: 28.04.2014 23:45
  */
 public class AddressSyncBean extends AbstractBean {
+    public final static String NS = AddressSyncBean.class.getName();
+
+    public <T extends AbstractAddressSync> void save(T addressSync){
+        if (addressSync.getId() == null){
+            sqlSession().insert(NS + ".insert" + addressSync.getClass().getSimpleName(), addressSync);
+        }
+    }
+
+    public <T extends AbstractAddressSync> T getObject(Class<T> objectClass, Long id){
+        return sqlSession().selectOne(NS + ".select" + objectClass.getSimpleName(), id);
+    }
+
+    public <T extends AbstractAddressSync> List<T> getList(Class<T> objectClass, FilterWrapper<T> filterWrapper){
+        return sqlSession().selectList(NS + ".select" + objectClass.getSimpleName() + "List", filterWrapper);
+    }
+
+    public <T extends AbstractAddressSync> Long getCount(Class<T> objectClass, FilterWrapper<T> filterWrapper){
+        return sqlSession().selectOne(NS + ".select"  + objectClass.getSimpleName() + "Count", filterWrapper);
+    }
+
+    public <T extends AbstractAddressSync> void delete(Class<T> objectClass, Long id){
+        sqlSession().delete(NS + ".delete" + objectClass.getSimpleName(), id);
+    }
 }
