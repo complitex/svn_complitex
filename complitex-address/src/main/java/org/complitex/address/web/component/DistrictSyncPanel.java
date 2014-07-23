9cbb8337e40b7f835a52d8dd1115d6abe479d1c8
heaven.iaa <heaven.iaa@gmail.com>
2014-07-23 13:04:11 +0000

    add district sync panel title

--- complitex-address/src/main/java/org/complitex/address/web/component/DistrictSyncPanel.java
+++ complitex-address/src/main/java/org/complitex/address/web/component/DistrictSyncPanel.java
@@ -7,6 +7,7 @@ import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.AjaxSelfUpdatingTimerBehavior;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.ResourceModel;
