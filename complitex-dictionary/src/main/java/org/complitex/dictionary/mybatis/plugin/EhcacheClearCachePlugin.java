c7f71d5728c05fe1b189dbc260b42929034f5f6c
pavel.sknar <pavel.sknar@gmail.com>
2014-07-30 03:50:43 +0000

    Work on performance: checking execute sql query

--- complitex-dictionary/src/main/java/org/complitex/dictionary/mybatis/plugin/EhcacheClearCachePlugin.java
+++ complitex-dictionary/src/main/java/org/complitex/dictionary/mybatis/plugin/EhcacheClearCachePlugin.java
@@ -1,24 +1,17 @@
 package org.complitex.dictionary.mybatis.plugin;
 
-import com.google.common.collect.Lists;
-import org.apache.ibatis.cache.CacheKey;
 import org.apache.ibatis.executor.Executor;
-import org.apache.ibatis.executor.SimpleExecutor;
 import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
 import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
 import org.apache.ibatis.plugin.Plugin;
 import org.apache.ibatis.plugin.Signature;
-import org.apache.ibatis.session.ResultHandler;
-import org.apache.ibatis.session.RowBounds;
 import org.complitex.dictionary.mybatis.caches.EhcacheCache;
 import org.complitex.dictionary.mybatis.caches.EhcacheTableService;
 import org.complitex.dictionary.mybatis.plugin.util.ExcludeNamespacePlugin;
-import org.complitex.dictionary.util.EjbBeanLocator;
 
 import javax.ejb.EJB;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -27,14 +20,14 @@ import java.util.regex.Pattern;
  */
 @Intercepts({@Signature(
         type= Executor.class,
-        method = "query",
-        args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class})})
-public class SelectStatementPluginExclude extends ExcludeNamespacePlugin {
+        method = "update",
+        args = {MappedStatement.class, Object.class})})
+public class EhcacheClearCachePlugin extends ExcludeNamespacePlugin {
+
     private final static int MAPPED_STATEMENT_INDEX = 0;
     private final static int PARAMETER_INDEX = 1;
-    private final static int ROW_BOUNDS_INDEX = 2;
 
-    private final static Pattern PATTERN = Pattern.compile("(?i)(from|join)\\s+['|`|\"]?(?<table>\\w+)\\W+");
+    private final static Pattern PATTERN = Pattern.compile("(?i)(insert\\s+into|update|delete\\s+from)\\s+['|`|\"]?(?<table>\\w+)\\W+");
 
     @EJB
     private EhcacheTableService ehcacheTableService;
@@ -43,40 +36,22 @@ public class SelectStatementPluginExclude extends ExcludeNamespacePlugin {
     public Object intercept(Invocation invocation) throws Throwable {
         MappedStatement ms = (MappedStatement)invocation.getArgs()[MAPPED_STATEMENT_INDEX];
         EhcacheCache mscache = (EhcacheCache)ms.getCache();
-
         if (mscache == null || namespaces.contains(mscache.getId())) {
             return invocation.proceed();
         }
-
         Object parameterObject = invocation.getArgs()[PARAMETER_INDEX];
-        RowBounds rowBounds = (RowBounds)invocation.getArgs()[ROW_BOUNDS_INDEX];
+        BoundSql boundSql = ms.getBoundSql(parameterObject);  // ms.getResource();
+        String tableName = getTableFromQuery(boundSql);
+        if (tableName != null) {
 
-        BoundSql boundSql = ms.getBoundSql(parameterObject);
-        List<String> tables = getTablesFromQuery(boundSql);
-
-        if (!tables.isEmpty()) {
-
-            SimpleExecutor executor = new SimpleExecutor(ms.getConfiguration(), null);
-
-            CacheKey cacheKey = executor.createCacheKey(ms, parameterObject, rowBounds, boundSql);
-
-            EhcacheTableService ehcacheTableService = EjbBeanLocator.getBean(EhcacheTableService.class);
-
-            ehcacheTableService.addCaches(tables, mscache.getInnerId(), cacheKey);
+            mscache.addTable(tableName);
         }
-
         return invocation.proceed();
     }
 
-    private List<String> getTablesFromQuery(BoundSql boundSql) {
+    private String getTableFromQuery(BoundSql boundSql) {
         Matcher matcher = PATTERN.matcher(boundSql.getSql());
-        int idx = 0;
-        List<String> result = Lists.newArrayList();
-        while (matcher.find(idx)) {
-            result.add(matcher.group("table"));
-            idx = matcher.end();
-        }
-        return result;
+        return matcher.find() ? matcher.group("table") : null;
     }
 
     @Override
